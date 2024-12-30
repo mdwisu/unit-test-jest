dc76db662019b34b19792f9e1051c1e5c9b76ee2
Muhammad Dwi Susanto <dwisusanto784@gmail.com>
2024-12-30 11:27:18 +0700

    ✨ feat(todoFuncton): todo untuk pengingat

---
+++ tests/16.todo-function.test.js
@@ -0,0 +1,4 @@
+// Gunakan Todo Function ketika kita berencana membuat unit test, namun dilakukan
+// Todo Function akan ditampilkan sebagai summary ketika kita menjalankan unit test, untuk mengingatkan kita
+
+it.todo("test todo function");
